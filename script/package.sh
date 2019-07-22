3deee90f696d48cb0902abc82fb22d3cc6637a1c
oneRain <onerain88@gmail.com>
2019-07-22 11:54:20 +0800

    chore: ci

--- script/package.sh
+++ script/package.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
+mkdir DLLs
+rsync -avz ./Storage/Storage.PCL/bin/Release/ DLLs
+rsync -avz ./RTM/RTM.PCL/bin/Release/ DLLs
+rsync -avz ./LiveQuery/LiveQuery.PCL/bin/Release/ DLLs
+zip -r LeanCloud-Portable-SDK.zip DLLs
+rm -r DLLs
+
 mkdir Plugins
-rsync -av --exclude='UnityEngine.dll' ./Storage/Storage.PCL/bin/Release/ ./Plugins/
-zip -r LeanCloud-CSharp-SDK.zip Plugins
+rsync -av --exclude='UnityEngine.dll' ./Storage/Storage.Unity/bin/Release/ Plugins
+rsync -av --exclude='UnityEngine.dll' ./RTM/RTM.Unity/bin/Release/ Plugins
+rsync -av --exclude='UnityEngine.dll' ./LiveQuery/LiveQuery.Unity/bin/Release/ Plugins
 zip -r LeanCloud-Unity-SDK.zip Plugins
 rm -r Plugins
