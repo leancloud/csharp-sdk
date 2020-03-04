7157230c9f96f7532f0dac8228a69b8ec9ba26e8
oneRain <onerain88@gmail.com>
2020-03-04 14:52:07 +0800

    chore: ci 脚本

--- script/package.sh
+++ script/package.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 mkdir DLLs
-rsync -avz ./Storage/Storage.PCL/bin/Release/ DLLs
-rsync -avz ./RTM/RTM.PCL/bin/Release/ DLLs
-rsync -avz ./LiveQuery/LiveQuery.PCL/bin/Release/ DLLs
-zip -r LeanCloud-Portable-SDK.zip DLLs
+rsync -avz ./Storage/bin/Release/netstandard2.0/ DLLs
+#rsync -avz ./RTM/RTM.PCL/bin/Release/ DLLs
+#rsync -avz ./LiveQuery/LiveQuery.PCL/bin/Release/ DLLs
+zip -r LeanCloud-SDK-Standard.zip DLLs
 rm -r DLLs
 
 mkdir Plugins
-rsync -av --exclude='UnityEngine.dll' ./Storage/Storage.Unity/bin/Release/ Plugins
-rsync -av --exclude='UnityEngine.dll' ./RTM/RTM.Unity/bin/Release/ Plugins
-rsync -av --exclude='UnityEngine.dll' ./LiveQuery/LiveQuery.Unity/bin/Release/ Plugins
-zip -r LeanCloud-Unity-SDK.zip Plugins
+rsync -av --exclude='UnityEngine.dll' ./Storage/bin/Release/netstandard2.0/ Plugins
+#rsync -av --exclude='UnityEngine.dll' ./RTM/RTM.Unity/bin/Release/ Plugins
+#rsync -av --exclude='UnityEngine.dll' ./LiveQuery/LiveQuery.Unity/bin/Release/ Plugins
+zip -r LeanCloud-SDK-Unity.zip Plugins
 rm -r Plugins
