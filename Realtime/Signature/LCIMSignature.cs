816e735968c96369dc326cc150b2827c52a98b6f
oneRain <onerain88@gmail.com>
2020-04-20 13:38:14 +0800

    * LCIMSignature.cs:
    * LCIMSignatureAction.cs: chore: 签名相关注释

--- Realtime/Signature/LCIMSignature.cs
+++ Realtime/Signature/LCIMSignature.cs
@@ -1,19 +1,27 @@
 ﻿namespace LeanCloud.Realtime {
+    /// <summary>
+    /// 签名数据
+    /// </summary>
     public class LCIMSignature {
+        /// <summary>
+        /// 签名
+        /// </summary>
         public string Signature {
             get; set;
         }
 
+        /// <summary>
+        /// 时间戳
+        /// </summary>
         public long Timestamp {
             get; set;
         }
 
+        /// <summary>
+        /// 随机字符串
+        /// </summary>
         public string Nonce {
             get; set;
-        }
-
-        public LCIMSignature() {
-            
         }
     }
 }
