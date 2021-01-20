e2b3f07643b8cae7aa439686b808879ec84da7d1
oneRain <onerain88@gmail.com>
2021-01-20 15:08:28 +0800

    chore: pack unity with link.xml

--- script/package.sh
+++ script/package.sh
@@ -4,20 +4,24 @@ pack() {
     local path=$1;
     local dir=$2;
     local output=$3;
+    local copyLink=$4;
     mkdir $dir
     rsync -avz $path $dir
+    if [ $copyLink == true ] ; then
+        cp ./Unity/link.xml $dir
+    fi
     zip -r $output $dir
     rm -r $dir
 }
 
 # Storage
 pack ./Storage/Storage/bin/Release/netstandard2.0/ ./DLLs LeanCloud-SDK-Storage-Standard.zip
-pack ./Storage/Storage-Unity/bin/Release/netstandard2.0/ ./Plugins LeanCloud-SDK-Storage-Unity.zip
+pack ./Storage/Storage-Unity/bin/Release/netstandard2.0/ ./Plugins LeanCloud-SDK-Storage-Unity.zip true
 
 # Realtime
 pack ./Realtime/Realtime/bin/Release/netstandard2.0/ ./DLLs LeanCloud-SDK-Realtime-Standard.zip
-pack ./Realtime/Realtime-Unity/bin/Release/netstandard2.0/ ./Plugins LeanCloud-SDK-Realtime-Unity.zip
+pack ./Realtime/Realtime-Unity/bin/Release/netstandard2.0/ ./Plugins LeanCloud-SDK-Realtime-Unity.zip true
 
 # LiveQuery
 pack ./LiveQuery/LiveQuery/bin/Release/netstandard2.0/ ./DLLs LeanCloud-SDK-LiveQuery-Standard.zip
-pack ./LiveQuery/LiveQuery-Unity/bin/Release/netstandard2.0/ ./Plugins LeanCloud-SDK-LiveQuery-Unity.zip
+pack ./LiveQuery/LiveQuery-Unity/bin/Release/netstandard2.0/ ./Plugins LeanCloud-SDK-LiveQuery-Unity.zip true
