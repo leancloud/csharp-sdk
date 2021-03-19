b8de05e867f45ea465f5cec264ab33cc338277d5
oneRain <onerain88@gmail.com>
2021-03-19 14:45:29 +0800

    chore: runtime version

--- Engine/Handlers/LCPingHandler.cs
+++ Engine/Handlers/LCPingHandler.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace LeanCloud.Engine {
     public class LCPingHandler {
         public static object HandlePing() {
             LCLogger.Debug("Ping ~~~");
             return new Dictionary<string, string> {
-                { "runtime", "dotnet-3.1" },
+                { "runtime", $"dotnet-{Environment.Version}" },
                 { "version", LCApplication.SDKVersion }
             };
         }
