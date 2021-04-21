84b008309aba6dc534b8e191cba7bf1bd86f8996
oneRain <onerain88@gmail.com>
2021-04-21 14:42:38 +0800

    Update Storage/Storage/Public/LCRole.cs
    Co-authored-by: Jang Rush <weakish@gmail.com>

--- Storage/Storage/Public/LCRole.cs
+++ Storage/Storage/Public/LCRole.cs
@@ -6,7 +6,7 @@
         public const string CLASS_NAME = "_Role";
 
         /// <summary>
-        /// The name of LCRole.
+        /// The name of a LCRole.
         /// </summary>
         public string Name {
             get {
