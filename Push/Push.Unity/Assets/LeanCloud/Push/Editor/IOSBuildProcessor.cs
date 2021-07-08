9253c641485f9a40321d405d5e00248a3a04939b
oneRain <onerain88@gmail.com>
2021-07-08 16:21:05 +0800

    chore: iOS build

--- Push/Push.Unity/Assets/LeanCloud/Push/Editor/IOSBuildProcessor.cs
+++ Push/Push.Unity/Assets/LeanCloud/Push/Editor/IOSBuildProcessor.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Reflection;
 using UnityEngine;
 using UnityEditor;
 using UnityEditor.Callbacks;
@@ -9,6 +10,10 @@ public static class IOSBuildProcessor {
 #if UNITY_IOS
     [PostProcessBuild(999)]
     public static void OnPostProcessBuild(BuildTarget buildTarget, string path) {
+        if (buildTarget != BuildTarget.iOS) {
+            return;
+        }
+
         // 开启远程推送宏
         string preprocessorPath = path + "/Classes/Preprocessor.h";
         string text = File.ReadAllText(preprocessorPath);
@@ -18,14 +23,30 @@ public static class IOSBuildProcessor {
         // 添加推送能力
         string pbxPath = PBXProject.GetPBXProjectPath(path);
         Debug.Log($"pbxPath: {pbxPath}");
-        PBXProject proj = new PBXProject();
-        proj.ReadFromFile(pbxPath);
+        PBXProject pbxProj = new PBXProject();
+        pbxProj.ReadFromFile(pbxPath);
 
-        string guid = proj.GetUnityMainTargetGuid();
+        string guid = pbxProj.GetUnityMainTargetGuid();
         Debug.Log($"guid: {guid}");
         string[] ids = Application.identifier.Split('.');
         string entitlementsPath = $"Unity-iPhone/{ids[ids.Length - 1]}.entitlements";
         Debug.Log($"entitlementsPath: {entitlementsPath}");
+
+        // 添加 Frameworks
+        string unityFrameworkTarget;
+
+        MethodInfo unityMainTargetGuidMethod = pbxProj.GetType().GetMethod("GetUnityMainTargetGuid");
+        MethodInfo unityFrameworkTargetGuidMethod = pbxProj.GetType().GetMethod("GetUnityFrameworkTargetGuid");
+
+        if (unityMainTargetGuidMethod != null && unityFrameworkTargetGuidMethod != null) {
+            unityFrameworkTarget = unityFrameworkTargetGuidMethod.Invoke(pbxProj, null) as string;
+        } else {
+            unityFrameworkTarget = pbxProj.TargetGuidByName("Unity-iPhone");
+        }
+        Debug.Log($"framework target: {unityFrameworkTarget}");
+        pbxProj.AddFrameworkToProject(unityFrameworkTarget, "UserNotifications.framework", true);
+
+        File.WriteAllText(pbxPath, pbxProj.WriteToString());
 
         ProjectCapabilityManager capManager = new ProjectCapabilityManager(pbxPath, entitlementsPath, null, guid);
         capManager.AddPushNotifications(true);
