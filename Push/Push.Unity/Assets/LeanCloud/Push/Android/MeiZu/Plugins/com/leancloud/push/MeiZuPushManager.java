d5f6e95a54f9008128bd68ab9482cae0d5f9ffb7
oneRain <onerain88@gmail.com>
2021-07-16 16:29:58 +0800

    chore: meizu support

---
+++ Push/Push.Unity/Assets/LeanCloud/Push/Android/MeiZu/Plugins/com/leancloud/push/MeiZuPushManager.java
@@ -0,0 +1,25 @@
+package com.leancloud.push.meizu;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.leancloud.push.Utils;
+import com.meizu.cloud.pushsdk.PushManager;
+import com.unity3d.player.UnityPlayer;
+
+public class MeiZuPushManager {
+    public static void registerMeiZuPush(String appId, String appKey) {
+        if (Utils.isNullOrEmpty(appId)) {
+            Log.e(Utils.TAG, "MeiZu appId is empty");
+            return;
+        }
+        if (Utils.isNullOrEmpty(appKey)) {
+            Log.e(Utils.TAG, "MeiZu appKey is empty");
+            return;
+        }
+
+        Context context = UnityPlayer.currentActivity.getApplicationContext();
+        PushManager.register(context, appId, appKey);
+        Utils.intentParser = new Utils.IntentParser();
+    }
+}
