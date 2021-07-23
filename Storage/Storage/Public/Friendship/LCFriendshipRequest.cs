1cafdfc71983d1add0138afb0c26bcddd2cb7811
oneRain <onerain88@gmail.com>
2021-07-23 18:21:57 +0800

    chore: LCFriendshipRequest api

--- Storage/Storage/Public/Friendship/LCFriendshipRequest.cs
+++ Storage/Storage/Public/Friendship/LCFriendshipRequest.cs
@@ -6,7 +6,28 @@
     public class LCFriendshipRequest : LCObject {
         public const string CLASS_NAME = "_FriendshipRequest";
 
+        public const int STATUS_PENDING = 0x01;
+        public const int STATUS_ACCEPTED = 0x02;
+        public const int STATUS_DECLINED = 0x04;
+        public const int STATUS_ANY = 0x07;
+
+        public LCUser User {
+            get => base["user"] as LCUser;
+        }
+
+        public LCUser Friend {
+            get => base["friend"] as LCUser;
+        }
+
+        public string Status {
+            get => base["status"] as string;
+        }
+
         public LCFriendshipRequest() : base(CLASS_NAME) {
+        }
+
+        public static LCQuery<LCFriendshipRequest> GetQuery() {
+            return new LCQuery<LCFriendshipRequest>(CLASS_NAME);
         }
     }
 }
