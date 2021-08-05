804501b96363d04b7d44d1e1d3dca98681476b84
oneRain <onerain88@gmail.com>
2021-08-05 14:21:50 +0800

    feat: fcm push

--- Push/Push.Unity/Assets/LeanCloud/Push/Android/Common/Plugins/com/leancloud/push/Utils.java
+++ Push/Push.Unity/Assets/LeanCloud/Push/Android/Common/Plugins/com/leancloud/push/Utils.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.util.Log;
 
 import com.unity3d.player.UnityPlayer;
--- Push/Push.Unity/Assets/LeanCloud/Push/Editor/AndroidBuildPipeline.cs
+++ Push/Push.Unity/Assets/LeanCloud/Push/Editor/AndroidBuildPipeline.cs
@@ -7,12 +7,23 @@ public class AndroidPostBuildProcessor : IPostGenerateGradleAndroidProject {
     int IOrderedCallback.callbackOrder => 0;
 
     void IPostGenerateGradleAndroidProject.OnPostGenerateGradleAndroidProject(string path) {
+        string LauncherPath = $"{path}/../launcher";
+        string LCAndroidPath = $"{Application.dataPath}/LeanCloud/Push/Android";
         // 拷贝华为推送配置文件
-        string hmsConfigPath = $"{Application.dataPath}/LeanCloud/Push/Android/HuaWei/hms/agconnect-services.json";
+        string HMSFilename = "agconnect-services.json";
+        string hmsConfigPath = $"{LCAndroidPath}/HuaWei/hms/{HMSFilename}";
         if (File.Exists(hmsConfigPath)) {
-            File.Copy(hmsConfigPath, $"{path}/../launcher/agconnect-services.json", true);
+            File.Copy(hmsConfigPath, $"{LauncherPath}/{HMSFilename}", true);
         } else {
-            Debug.LogWarning($"No agconnect-services.json in {hmsConfigPath} for hms.");
+            Debug.LogWarning($"No {HMSFilename} in {hmsConfigPath} for hms.");
+        }
+        // 拷贝 FCM 推送配置文件
+        string FCMFilename = "google-services.json";
+        string fcmConfigPath = $"{LCAndroidPath}/FCM/config/{FCMFilename}";
+        if (File.Exists(fcmConfigPath)) {
+            File.Copy(fcmConfigPath, $"{LauncherPath}/{FCMFilename}", true);
+        } else {
+            Debug.LogWarning($"No {FCMFilename} in {fcmConfigPath} for fcm.");
         }
     }
 }
