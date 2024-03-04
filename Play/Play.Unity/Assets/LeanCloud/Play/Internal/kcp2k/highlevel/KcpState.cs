4b941bb19bf83dac05e97bef0c831bb818685226
pzheng <zapcannon87@outlook.com>
2024-03-04 17:08:19 +0800

    update kcp

---
+++ Play/Play.Unity/Assets/LeanCloud/Play/Internal/kcp2k/highlevel/KcpState.cs
@@ -0,0 +1,4 @@
+namespace LeanCloud.Play.kcp2k
+{
+    public enum KcpState { Connected, Authenticated, Disconnected }
+}
---
+++ Play/Play.Unity/Assets/LeanCloud/Play/Internal/kcp2k/kcp/AckItem.cs
@@ -0,0 +1,8 @@
+namespace LeanCloud.Play.kcp2k
+{
+    internal struct AckItem
+    {
+        internal uint serialNumber;
+        internal uint timestamp;
+    }
+}
